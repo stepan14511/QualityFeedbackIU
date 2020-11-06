893bab3c58e8433a6d1fd74fefe7ea751b03a1dc
datapaf <georgyandryuschenko@gmail.com>
2020-11-06 13:04:12 +0300

    Feedback Form Creation Done (with "data" field)

--- qualityfeedback/feedback_form_creation/urls.py
+++ qualityfeedback/feedback_form_creation/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,4 +21,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    url(r'^thanks/$', views.thanks, name='thanks'),
 ]
--- qualityfeedback/feedback_form_creation/views.py
+++ qualityfeedback/feedback_form_creation/views.py
@@ -1,22 +1,30 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import CreateFeedbackForm
+from feedbacks.models import Feedback
+from webapp.models import Group
 
 def index(request):
-	# _id = feedback_id
-	# feedback = Feedback.objects.get(id=_id)
-	# if request.method == "POST":
-	# 	form = FeedbackForm(request.POST)
-	# 	if form.is_valid():
-	# 		data = []
-	# 		for i in request.POST.keys():
-	# 			if i != 'csrfmiddlewaretoken':
-	# 				data.append([i, request.POST.getlist(i)])
-	# 		Answer.objects.create(feedback_rel=feedback, data=json.dumps(data))
-	# 		return HttpResponseRedirect('/feedbacks/thanks/')
+
+	if request.method == "POST":
+		form = CreateFeedbackForm(request.POST)
+		if form.is_valid():
+			data = []
+			for i in request.POST.keys():
+				if i != 'csrfmiddlewaretoken':
+					data.append(request.POST.get(i))
+			title = data[0]
+			group = Group.objects.get(group_name=data[1])
+			questions = data[2]
+			Feedback.objects.create(name=title, group_rel=group, data=questions)
+			return HttpResponseRedirect('/create/thanks/')
 
 	context = {
 		'title': 'Create a Feedback Form',
 		'form': CreateFeedbackForm()
 	}
 	return render(request, "form_creation/index.html", context)
+
+def thanks(request):
+	return render(request, 'form_creation/done_form_creation.html')
