df17339c2b5257e83827da56d2096238eb3e4b4c
Stepan Kuznetsov <stepan14511@gmail.com>
2020-11-08 00:43:56 +0300

    Added feature adding questions while creating a form.

--- qualityfeedback/feedback_form_creation/forms.py
+++ qualityfeedback/feedback_form_creation/forms.py
@@ -11,4 +11,4 @@ class CreateFeedbackForm(forms.Form):
 
     group_rel = forms.ChoiceField(label="Group rel", choices=CHOICES, widget=forms.Select(attrs={'class': 'ftype_single_choice'}))
 
-    data = forms.CharField(label="data", widget=forms.Textarea(attrs={'class' : 'ftype_textarea'}))
+    #data = forms.CharField(label="data", widget=forms.Textarea(attrs={'class' : 'ftype_textarea'}))
