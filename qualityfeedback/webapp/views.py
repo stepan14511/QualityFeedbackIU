d5ab5106412c3dfe6ee804c4a6947a2ca8d0ffae
datapaf <georgyandryuschenko@gmail.com>
2020-11-19 15:54:38 +0300

    list of all feedback forms in the beginning

--- qualityfeedback/webapp/views.py
+++ qualityfeedback/webapp/views.py
@@ -3,44 +3,66 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
 
-from .models import Course
+from .models import Course, Group
+from feedbacks.models import Feedback
 
 def index(request):
-	login_form = AuthenticationForm()
-	login_form.fields['username'].widget.attrs['class'] = 'center_h input_field'
-	login_form.fields['username'].widget.attrs['style'] = 'margin-top: 20px;'
-	login_form.fields['password'].widget.attrs['class'] = 'center_h input_field'
-	login_form.fields['password'].widget.attrs['style'] = 'margin-top: 5px;'
-	return render(request, 'index.html', {'login_form':login_form})
+    login_form = AuthenticationForm()
+    login_form.fields['username'].widget.attrs['class'] = 'center_h input_field'
+    login_form.fields['username'].widget.attrs['style'] = 'margin-top: 20px;'
+    login_form.fields['password'].widget.attrs['class'] = 'center_h input_field'
+    login_form.fields['password'].widget.attrs['style'] = 'margin-top: 5px;'
+    return render(request, 'index.html', {'login_form':login_form})
 
 def login(request):
-	if request.method == "POST":
-		login_form = AuthenticationForm(request=request, data=request.POST)
-		if login_form.is_valid():
-			auth.login(request, login_form.get_user())
-			return HttpResponseRedirect('/home')
-		else:
-			print(login_form.get_invalid_login_error())
-			return HttpResponseRedirect('/')
-	else:
-		return HttpResponseRedirect('/')
+    if request.method == "POST":
+        login_form = AuthenticationForm(request=request, data=request.POST)
+        if login_form.is_valid():
+            auth.login(request, login_form.get_user())
+            return HttpResponseRedirect('/home')
+        else:
+            print(login_form.get_invalid_login_error())
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
 
 def home(request):
-	user = auth.get_user(request)
-	if user.is_authenticated:
-		if user.role == 'professor':
-			context = {
-				'courses': get_professors_courses(user)
-			}
-			return render(request, 'home/professor.html', context)
-		else:
-			return HttpResponse("Hello, " + request.user.username)
-	else:
-		return HttpResponseRedirect('/')
+    user = auth.get_user(request)
+    if user.is_authenticated:
+        if user.role == 'professor':
+            print(get_professors_feedbackforms_by_group(user))
+            context = {
+                'feedbackforms_pool' : get_professors_feedbackforms(user),
+                'courses': get_professors_courses(user),
+                'feedbackforms_by_group' : get_professors_feedbackforms_by_group(user)
+            }
+            return render(request, 'home/professor.html', context)
+        else:
+            return HttpResponse("Hello, " + request.user.username)
+    else:
+        return HttpResponseRedirect('/')
 
 def get_professors_courses(professor):
-	professors_courses = []
-	for course in Course.objects.all():
-		if course.professor == professor:
-			professors_courses.append(course)
-	return professors_courses
+    professors_courses = []
+    for course in Course.objects.all():
+        if course.professor == professor:
+            professors_courses.append(course)
+    return professors_courses
+
+def get_professors_feedbackforms(professor):
+    feedbackforms = []
+    for feedbackform in Feedback.objects.all():
+        if feedbackform.group_rel.course_rel.professor == professor:
+            feedbackforms.append(feedbackform)
+    return feedbackforms
+
+def get_professors_feedbackforms_by_group(professor):
+    feedbackforms = []
+    for group in Group.objects.all():
+        if group.course_rel.professor == professor:
+            feedbackforms_for_group = []
+            for feedbackform in Feedback.objects.all():
+                if feedbackform.group_rel == group:
+                    feedbackforms_for_group.append(feedbackform)
+            feedbackforms.append((group, feedbackforms_for_group))
+    return feedbackforms
