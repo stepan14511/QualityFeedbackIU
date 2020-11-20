701e54671b474d57b1d95d7877d071c59bd456a2
datapaf <georgyandryuschenko@gmail.com>
2020-11-20 19:23:04 +0300

    showing list of questions of a feedback form on a separate page

--- qualityfeedback/feedbacks/views.py
+++ qualityfeedback/feedbacks/views.py
@@ -2,28 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 import json
 
+from django.contrib import auth
+
 from .models import Feedback, Answer
 from .forms import FeedbackForm
 
 def index(request, feedback_id):
-	_id = feedback_id
-	feedback = Feedback.objects.get(id=_id)
-	if request.method == "POST":
-		form = FeedbackForm(request.POST)
-		if form.is_valid():
-			data = []
-			for i in request.POST.keys():
-				if i != 'csrfmiddlewaretoken':
-					data.append( [i, request.POST.getlist(i)] )
-			Answer.objects.create(feedback_rel=feedback, data=json.dumps(data))
-			return HttpResponseRedirect('/feedbacks/thanks/')
-	form = FeedbackForm(data=json.loads(feedback.data))
-	context = {
-		'title':'Quality Feedback Service',
-		'name': feedback.name,
-		'feedback': form
-	}
-	return render(request, "form/index.html", context)
+	user = auth.get_user(request)
+	if user.is_authenticated:
+		if user.role == 'professor':
+			feedback = Feedback.objects.get(id=feedback_id)
+			context = {
+				'feedback_name' : feedback.name,
+				'feedback_items' : json.loads(feedback.data)
+			}
+			return render(request, "feedback_data/feedback_data.html", context)
+		else:
+			_id = feedback_id
+			feedback = Feedback.objects.get(id=_id)
+			if request.method == "POST":
+				form = FeedbackForm(request.POST)
+				if form.is_valid():
+					data = []
+					for i in request.POST.keys():
+						if i != 'csrfmiddlewaretoken':
+							data.append([i, request.POST.getlist(i)])
+					Answer.objects.create(feedback_rel=feedback, data=json.dumps(data))
+					return HttpResponseRedirect('/feedbacks/thanks/')
+			form = FeedbackForm(data=json.loads(feedback.data))
+			context = {
+				'title': 'Quality Feedback Service',
+				'name': feedback.name,
+				'feedback': form
+			}
+			return render(request, "form/index.html", context)
+	else:
+		return HttpResponseRedirect('/')
 
 def thanks(request):
 	return render(request, 'form/done.html')
