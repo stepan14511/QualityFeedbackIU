7493cdee5906f84866b0f44f4ae395e87f2998c7
datapaf <georgyandryuschenko@gmail.com>
2020-11-21 11:53:04 +0300

    showing answers and questions completed

--- qualityfeedback/feedbacks/views.py
+++ qualityfeedback/feedbacks/views.py
@@ -12,14 +12,15 @@ def index(request, feedback_id):
 	if user.is_authenticated:
 		if user.role == 'professor':
 			feedback = Feedback.objects.get(id=feedback_id)
-			answers = []
+			raw_answers = []
 			for answer in Answer.objects.all():
 				if answer.feedback_rel == feedback:
-					answers.append(json.loads(answer.data))
+					raw_answers.append(json.loads(answer.data))
+			raw_feedback_questions = json.loads(feedback.data)
+			print(compose_feedback_data(raw_feedback_questions, raw_answers))
 			context = {
 				'feedback_name' : feedback.name,
-				'feedback_questions' : json.loads(feedback.data),
-				'answers' : answers
+				'feedback_data' : compose_feedback_data(raw_feedback_questions, raw_answers)
 			}
 			return render(request, "feedback_data/feedback_data.html", context)
 		else:
@@ -43,6 +44,34 @@ def index(request, feedback_id):
 			return render(request, "form/index.html", context)
 	else:
 		return HttpResponseRedirect('/')
+
+def compose_feedback_data(feedback_questions, raw_answers):
+	answers = []
+	for feedback_question in feedback_questions:
+		answer = []
+		answer.append(feedback_question[1])
+		for raw_answer in raw_answers:
+			for raw_answer_to_question in raw_answer:
+				if raw_answer_to_question[0] == feedback_question[0]:
+					if feedback_question[2] == "text":
+						text_answer = []
+						text_answer.append(raw_answer_to_question[1][0])
+						answer.append(text_answer)
+					elif feedback_question[2] == "single_choice":
+						choice_number = int(raw_answer_to_question[1][0])
+						single_choice_answer = []
+						single_choice_answer.append(feedback_question[3][choice_number])
+						answer.append(single_choice_answer)
+					elif feedback_question[2] == "multiple_choice":
+						multiple_choice_answers = []
+						for raw_choice_number in raw_answer_to_question[1]:
+							choice_number = int(raw_choice_number)
+							multiple_choice_answers.append(feedback_question[3][choice_number])
+						answer.append(multiple_choice_answers)
+					elif feedback_question[2] == "slider":
+						answer.append(list(raw_answer_to_question[1][0]))
+		answers.append(answer)
+	return answers
 
 def thanks(request):
 	return render(request, 'form/done.html')
